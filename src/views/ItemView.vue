d04c7aa7ac2d168826fd7fe36b59fe27aca07390
Jaehyeokk <lionjk92@naver.com>
2021-06-07 22:58:44 +0900

    Install and setup vue-router

---
+++ src/views/ItemView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    ItemView
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    JobsView
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
