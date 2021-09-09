2f2d8a7e3f4b1126353ba94ea372cfcc1729a67e
Jaehyeokk <lionjk92@naver.com>
2021-09-09 22:30:56 +0900

    Adding ESLint prettier

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,57 +1,57 @@
 <template>
-  <div class="user-page">
-    <div class="container">
-      <UserProfile>
-        <template slot="avatar"><i class="fas fa-user"></i></template>
-        <template slot="name">ID: {{ this.user_info.id }}</template>
-        <template slot="karma">Karma: {{ this.user_info.karma }}</template>
-        <template slot="created"
-          >Created: {{ this.user_info.created }}</template
-        >
-      </UserProfile>
-      <div class="user-dec" v-html="user_info.about"></div>
-    </div>
-  </div>
+	<div class="user-page">
+		<div class="container">
+			<UserProfile>
+				<template slot="avatar"><i class="fas fa-user"></i></template>
+				<template slot="name">ID: {{ this.user_info.id }}</template>
+				<template slot="karma">Karma: {{ this.user_info.karma }}</template>
+				<template slot="created"
+					>Created: {{ this.user_info.created }}</template
+				>
+			</UserProfile>
+			<div class="user-dec" v-html="user_info.about"></div>
+		</div>
+	</div>
 </template>
 
 <script>
-import bus from "../util/bus.js";
-import { mapState } from "vuex";
-import UserProfile from "../components/UserProfile.vue";
+import bus from '../util/bus.js';
+import { mapState } from 'vuex';
+import UserProfile from '../components/UserProfile.vue';
 export default {
-  components: {
-    UserProfile,
-  },
-  computed: {
-    ...mapState(["user_info"]),
-  },
-  created() {
-    bus.$emit("start:spinner");
-    this.$store
-      .dispatch("FETCH_USER_INFO", this.$route.params.id)
-      .then(() => bus.$emit("stop:spinner"))
-      .catch((e) => console.log(e));
-  },
+	components: {
+		UserProfile,
+	},
+	computed: {
+		...mapState(['user_info']),
+	},
+	created() {
+		bus.$emit('start:spinner');
+		this.$store
+			.dispatch('FETCH_USER_INFO', this.$route.params.id)
+			.then(() => bus.$emit('stop:spinner'))
+			.catch(e => console.log(e));
+	},
 };
 </script>
 
 <style scoped>
 .container {
-  width: 1080px;
-  padding: 20px;
-  box-sizing: border-box;
+	width: 1080px;
+	padding: 20px;
+	box-sizing: border-box;
 }
 
 .user-dec {
-  padding: 10px;
-  margin-top: 30px;
-  border: 1px solid #333;
-  line-height: 1.3;
+	padding: 10px;
+	margin-top: 30px;
+	border: 1px solid #333;
+	line-height: 1.3;
 }
 
 @media screen and (max-width: 1080px) {
-  .container {
-    width: 100%;
-  }
+	.container {
+		width: 100%;
+	}
 }
 </style>
